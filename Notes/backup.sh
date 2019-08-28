c0392ccc576ccaa88b46bb6020f470c77f00a50c
MairusuPawa <mprower@gmail.com>
2019-08-28 16:14:13 +0200

    [GB] Brouillon script backup

--- Notes/backup.sh
+++ Notes/backup.sh
@@ -4,7 +4,7 @@
 #
 # Ce script doit tourner sur le serveur de backup.
 # On recupère une copie de la BDD de prod via le reseau.
-# Necessite mysqldump (du paquet mysql) et rsync.
+# Necessite mysqldump (du paquet mysql) et gzip.
 # 
 ################################
 
@@ -23,24 +23,34 @@ timestamp_day=$(date '+%Y%m%d')
 timestamp=$(date '+%Y%m%d-%H%M%S')
 
 # Fonctions
-maFonction ()
+VerifTailleDispo ()
 {
-  bloc d’instructions 
+  df $1 | awk '{ print $5 }' | tail -n 1| cut -d'%' -f1
 }
 
+VerifDossier ()
+{
+  if [ -d $1 ] 
+  then
+      true
+  else
+    echo "[$timestamp] *** Le dossier $1 est injoignable" >> $dir_log/backup-$timestamp_day.log
+    echo "[$timestamp] *** Could not reach directory $1" >> $dir_log/backup-critical.log
+    exit 1
+  fi
+}
 
 ### Verification de l environnement
-
 # Journalisation
-if [[ -f $dir_log/backup-critical.log ]];
+if [[ -f $dir_log/backup-critical.log ]]
 then
   true
 else
   touch /$dir_log/backup-critical.log
-  echo "[$timestamp] Creation of the critical errors log file" >> $dir_log/backup-$timestamp_day.log
+  echo "[$timestamp] Creation of the critical errors log file" >> $dir_log/backup-critical.log
 fi
 
-if [[ -f $dir_log/backup-$timestamp_day.log ]];
+if [[ -f $dir_log/backup-$timestamp_day.log ]]
 then
   echo "[$timestamp] * Le script a ete execute de nouveau" >> $dir_log/backup-$timestamp_day.log
 else
@@ -49,32 +59,16 @@ else
 fi
 
 # Volumes et points de montage
-if [ -d $dir_temp ] 
-then
-    true
-else
-  echo "[$timestamp] *** Le dossier temporaire est injoignable" >> $dir_log/backup-$timestamp_day.log
-  echo "[$timestamp] *** Could not reach temporary files directory" >> $dir_log/backup-critical.log
-  exit 1
-fi
+VerifDossier $dir_temp
+VerifDossier $dir_backup
 
-if [ -d $dir_backup ] 
-then
-    true
-else
-  echo "[$timestamp] Le chemin $dir_backup est injoignable" >> $dir_log/backup-$timestamp_day.log
-  echo "[$timestamp] *** Could not reach backup directory" >> $dir_log/backup-critical.log
-  exit 1
-fi
-
-
-
-
+VerifTailleDispo $dir_temp
+VerifTailleDispo $dir_backup
 
 ### Recuperation de la base
 mysqldump --host $bdd_ip -P $bdd_port -u $bdd_user -p $bdd_password --single-transaction --quick --lock-tables=false $bdd_name > $dir_temp/infinivo-$timestamp.sql
 
-if [[ $? -eq 0 ]];
+if [[ $? -eq 0 ]]
 then
   echo "[$timestamp] Base MySQL OK" >> $dir_log/backup-$timestamp_day.log
 else
@@ -86,3 +80,16 @@ else
 fi
 
 # Traitement du fichier
+gzip -q $dir_temp/infinivo-$timestamp.sql
+mv $dir_temp/infinivo-$timestamp.sql.gz $dir_backup
+
+if [ $? -eq 0 ]
+then
+  rm $dir_temp/infinivo-$timestamp.sql.gz
+else
+  echo "[$timestamp] *** ERREUR CRITIQUE FS BACKUP ***" >> $dir_log/backup-$timestamp_day.log
+  echo "[$timestamp] *** CRITICAL BACKUP FILESYSTEM ERROR ***" >> $dir_log/backup-critical.log
+  exit 1
+fi
+
+echo "[$timestamp] *** Traitement OK ***" >> $dir_log/backup-$timestamp_day.log
