2ba46a7cf46426b6ce87d6b1d300b4f8768a9fda
Amelie <amelie.lpe@sfr.fr>
2019-08-28 16:31:45 +0200

    [AL] debug

--- Scripts/random_data.sh
+++ Scripts/random_data.sh
@@ -42,8 +42,8 @@ EOFMYSQL
 EOFMYSQL
 
     # Table licence
-    logiciel_id=`shuf -i 1-400 -n 1`
-    date_debut=`date -d "$((RANDOM%6+2012))-$((RANDOM%12+1))-$((RANDOM%28+1))" '+%Y-%m-%d'`
+    logiciel_id=`shuf -i 1-$x -n 1`
+    date_debut=`date -d "$((RANDOM%6+2006))-$((RANDOM%12+1))-$((RANDOM%28+1))" '+%Y-%m-%d'`
     date_fin=`date -d "$((RANDOM%2+2012))-$((RANDOM%12+1))-$((RANDOM%28+1))" '+%Y-%m-%d'`
     utilisation=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 3 | head -n 1)
 
@@ -54,10 +54,10 @@ EOFMYSQL
 EOFMYSQL
 
     # table commande
-    client_id=`shuf -i 1-400 -n 1`
+    client_id=`shuf -i 1-$x -n 1`
     date_commande=`date -d "$((RANDOM%6+2010))-$((RANDOM%12+1))-$((RANDOM%28+1))" '+%Y-%m-%d'`
-    logiciel_id=`shuf -i 1-400 -n 1`
-    licence_id=`shuf -i 1-400 -n 1`
+    logiciel_id=`shuf -i 1-$x -n 1`
+    licence_id=`shuf -i 1-$x -n 1`
     remise=`shuf -i 1-100 -n 1`
     quantite=`shuf -i 1-12 -n 1`
     prix=`shuf -i 1-6000 -n 1`
