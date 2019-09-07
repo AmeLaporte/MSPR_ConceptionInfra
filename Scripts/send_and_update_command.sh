4db9e5a0aa505d8e71e63be6ffed6876beb692c7
Amelie <amelie.lpe@sfr.fr>
2019-09-07 20:55:41 +0200

    [AL] improvment

--- Scripts/send_and_update_command.sh
+++ Scripts/send_and_update_command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-commandes=$(mysql -D test_infinivo -u amelie -ppassword -se "select logiciel_id from commande where status_com="N")
+commandes=$(mysql -D test_infinivo -u amelie -ppassword -se "select logiciel_id from commande where status_com='N'")
 
 for logiciel in $commandes
 do
