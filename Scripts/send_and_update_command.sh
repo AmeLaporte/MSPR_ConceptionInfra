cee4351b57226572bd1791d36a26f87d78712447
Amelie <amelie.lpe@sfr.fr>
2019-09-07 20:49:44 +0200

    [AL] improvmentz

--- Scripts/send_and_update_command.sh
+++ Scripts/send_and_update_command.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-commandes=$(mysql -D test_infinivo -u amelie -ppassword -se "select * from commande where status="N")
+commandes=$(mysql -D test_infinivo -u amelie -ppassword -se "select logiciel_id from commande where status_com="N")
+
+for logiciel in $commandes
+do
+    available_lic=$(mysql -D test_infinivo -u amelie -ppassword -se "select licence_id where status_lic='N' and logiciel_id=$logiciel")
+    echo $commande
+    echo $available_lic
+done
